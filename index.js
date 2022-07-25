44201a28cc11e33e32fe7c1635de81a4434c9d0d
Lindokuhle777 <92369482+Lindokuhle777@users.noreply.github.com>
2022-07-25 16:08:36 +0200

    created courses route

--- index.js
+++ index.js
@@ -160,6 +160,38 @@ app.post("/getErolledCourses", async (req, res) => {
   res.send(coursesArr);
 });
 
+app.post("/getCreatedCourses",async(req, res)=>{
+  const userID = req.body.userID;
+
+  const userRef = doc(db, "Users", userID);
+
+  const coursesSnap = await getDoc(userRef);
+
+  const coursesArrSnap = coursesSnap.data().created;
+
+  let coursesArr = [];
+
+  for (let i = 0; i < coursesArrSnap.length; i++) {
+    const currCourse = coursesArrSnap[i];
+
+    const courseRef = doc(db, "Courses", currCourse);
+
+    const courseData = await getDoc(courseRef);
+    const tempData = {
+      courseID: courseData.data().courseID,
+      courseName: courseData.data().courseName,
+      images: courseData.data().images,
+    };
+    coursesArr.push(tempData);
+  }
+  console.log(coursesArr);
+
+  res.send(coursesArr);
+
+
+
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
