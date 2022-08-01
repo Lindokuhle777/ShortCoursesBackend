0399dea22213232eda1768d7757305dba3d1842f
Lindokuhle777 <92369482+Lindokuhle777@users.noreply.github.com>
2022-08-01 16:51:30 +0200

    new course route plus slides

--- routes/newCourse.js
+++ routes/newCourse.js
@@ -10,16 +10,16 @@ import {
   getDoc,
 } from "firebase/firestore";
 import { db } from "../firebase-config.js";
-
 const router = express.Router();
-router.post("/", async (req, res) => {
-  const courseID = req.body.courseID;
-  const courseName = req.body.courseName;
-  const duration = req.body.duration;
-  const userID = req.body.creator;
-  const outcomes = req.body.outcomes;
-  const images = req.body.images;
-  const description = req.body.description;
+
+const handleCourseData = async (courseData) => {
+  const courseID = courseData.courseID;
+  const courseName = courseData.courseName;
+  const duration = courseData.duration;
+  const userID = courseData.userID;
+  const outcomes = courseData.outcomes;
+  const images = courseData.images;
+  const description = courseData.description;
 
   const courseRef = doc(db, "Courses", courseID);
 
@@ -41,8 +41,17 @@ router.post("/", async (req, res) => {
   const userSnap = await getDoc(userRef);
 
   await updateDoc(userRef, { created: [...userSnap.data().created, courseID] });
+};
 
-  res.send("Course created");
+const handleSlideData = async (slideData) => {
+  await setDoc(doc(db, "Slides", slideData.courseID), slideData);
+};
+
+router.post("/", async (req, res) => {
+  await handleCourseData(req.body.courseData);
+  await handleSlideData(req.body.slideData);
+
+  res.send("course created");
 });
 
 export default router;
